a41e4e00b3de2dde497f928f5b9d927274ad30fb
Reyypo <yogaajip409@gmail.com>
2026-06-12 01:50:39 +0700

    add gif embed header

--- rolePanel.js
+++ rolePanel.js
@@ -9,6 +9,8 @@ const {
 
 const ROLE_PANEL_CHANNEL_ID = process.env.ROLE_PANEL_CHANNEL_ID;
 
+const HEADER_GIF = 'https://cdn.discordapp.com/attachments/1514363278615380281/1514696314208653544/standard_7.gif?ex=6a2c4e5f&is=6a2afcdf&hm=e869f203766df2fa2336490f93cdff47d8562330ed7eeb75fab01e6355c09652&';
+
 const pendingGenderChoices = new Map();
 const pendingGameChoices = new Map();
 
@@ -34,11 +36,12 @@ async function setupRolePanel(client) {
   const channel = await client.channels.fetch(ROLE_PANEL_CHANNEL_ID);
 
   const oldMessages = await channel.messages.fetch({ limit: 50 });
+
   const alreadyExists = oldMessages.some(msg =>
-  msg.author.id === client.user.id &&
-  msg.embeds.length > 0 &&
-  msg.components.length > 0
-);
+    msg.author.id === client.user.id &&
+    msg.embeds.length > 0 &&
+    msg.components.length > 0
+  );
 
   if (alreadyExists) {
     console.log('Role panel sudah ada, tidak kirim ulang');
@@ -46,10 +49,10 @@ async function setupRolePanel(client) {
   }
 
   const genderEmbed = new EmbedBuilder()
-  .setAuthor({
-  name: 'REST AREA',
-  iconURL: 'https://cdn.discordapp.com/attachments/1514363278615380281/1514696314208653544/standard_7.gif?ex=6a2c4e5f&is=6a2afcdf&hm=e869f203766df2fa2336490f93cdff47d8562330ed7eeb75fab01e6355c09652&'
-})
+    .setAuthor({
+      name: 'REST AREA',
+      iconURL: HEADER_GIF
+    })
     .setDescription('**Pilih gender kamu, lalu klik ✅ Simpan Gender.**')
     .setColor(0x3498db)
     .setImage('https://cdn.discordapp.com/attachments/1514363278615380281/1514383535916716232/standard_2.gif?ex=6a2b2b12&is=6a29d992&hm=93661b6757ab76125c8f34d5e80b1d6c5168e4c8f8eafaf2994e9131e853ca79&');
@@ -70,10 +73,10 @@ async function setupRolePanel(client) {
     .setStyle(ButtonStyle.Danger);
 
   const gameEmbed = new EmbedBuilder()
-  .setAuthor({
-  name: 'REST AREA',
-  iconURL: 'https://cdn.discordapp.com/attachments/1514363278615380281/1514696314208653544/standard_7.gif?ex=6a2c4e5f&is=6a2afcdf&hm=e869f203766df2fa2336490f93cdff47d8562330ed7eeb75fab01e6355c09652&'
-})
+    .setAuthor({
+      name: 'REST AREA',
+      iconURL: HEADER_GIF
+    })
     .setDescription('**Pilih game yang kamu mainkan, lalu klik ✅ Simpan Pilihan.**')
     .setColor(0x3498db)
     .setImage('https://cdn.discordapp.com/attachments/1514363278615380281/1514394569469333566/standard_5.gif?ex=6a2b3559&is=6a29e3d9&hm=e5c250eb89f08ce8498a748732e24737a451a2f96f4a3bed7a8f8aede5163c99&');
